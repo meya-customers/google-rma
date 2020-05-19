37435856ec86d53866f5356414ad81a5ae8551b0
Dan Cecile <dan@meya.ai>
2020-05-19 16:17:12 -0400

    Fix submit RMA component & test 🔧

--- component/submit_rma_test.py
+++ component/submit_rma_test.py
@@ -1,75 +1,40 @@
 import pytest
 
-from component.submit_rma import RmaComponentElement
-from meya.component.entry.start import ComponentStartEntry
-from meya.db.model.user import UserModel
-from meya.element.element_test import to_spec_dict
+from component.submit_rma import SubmitRmaComponent
+from meya.element.element_test import create_component_start_entry
+from meya.element.element_test import create_flow_next_entry
+from meya.element.element_test import create_log_message_entry
+from meya.element.element_test import create_user
 from meya.element.element_test import verify_process_element
-from meya.flow.entry.next import FlowNextEntry
-from meya.log.entry.message import LogMessageEntry
 from meya.log.level import Level
-from meya.log.scope import Scope
-from meya.orb.composer_spec import ComposerSpec
+from meya.orb.composer_spec import ComposerEventSpec
 from meya.text.event.say import SayEvent
 from meya.util.generate_id import generate_member_id
-from unittest.mock import MagicMock
 
 
-# example test...remove `_x` to enable
-@pytest.mark.skip(reason="Need to load element from TypeRegistry")
 @pytest.mark.asyncio
-async def _xtest_submit_rma_component():
-    bot_id = "test_bot"
-    component = RmaComponentElement()
-    stack = []
-    data = {"foo": "bar"}
-    flow = "rma_test"
+async def test_submit_rma_component():
+    component = SubmitRmaComponent()
     firstname = "Firstname"
     lastname = "Lastname"
-    start_component_entry = ComponentStartEntry(
-        bot_id=bot_id,
-        spec=to_spec_dict(component),
-        data=data,
-        flow=flow,
-        index=0,
-        stack=stack,
-        thread_id="t-0",
-    )
-    component.entry = start_component_entry
+    component_start_entry = create_component_start_entry(component)
     text_event = SayEvent(
-        composer=ComposerSpec(),
-        member_id=generate_member_id(bot_id),
+        composer=ComposerEventSpec(),
+        member_id=generate_member_id(component_start_entry.bot_id),
         quick_replies=[],
         text="Submitted your RMA, your reference ~0",
-        thread_id="t-0",
+        thread_id=component_start_entry.thread_id,
     )
-    next_entry = FlowNextEntry(
-        bot_id=bot_id,
-        data=data,
-        flow=flow,
-        index=0,
-        stack=stack,
-        thread_id="t-0",
-    )
-    log_entry = LogMessageEntry(
-        args=[],
-        context={
-            "firstname": firstname,
-            "lastname": lastname,
-            "sub_entry": {"id": None, "type": "engine.meya.component.start"},
-        },
+    next_entry = create_flow_next_entry(component_start_entry)
+    log_entry = create_log_message_entry(
+        component_start_entry,
         level=Level.INFO,
         message="Submitted RMS",
-        scope=Scope.SYSTEM,
-        timestamp=1561492810010,
+        context={"firstname": firstname, "lastname": lastname},
     )
     await verify_process_element(
-        element=component,
-        sub_entry=start_component_entry,
-        expected_pub_entries=[text_event, next_entry, log_entry],
-        bot_id=bot_id,
-        user=UserModel(
-            __user_id__=MagicMock(),
-            __data__=dict(firstname=firstname, lastname=lastname),
-        ),
+        component,
+        component_start_entry,
+        [text_event, next_entry, log_entry],
+        user=create_user(data=dict(firstname=firstname, lastname=lastname)),
     )
